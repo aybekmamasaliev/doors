8ebd5c2b6b8f37497c5185d401aa328bdbb08e73
aybek <aybek.mamasaliev@bk.ru>
2023-02-10 20:13:43 +0600

    feat: add review list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form/Form';
+import ReviewItem from './components/ReviewItem/ReviewItem';
+import Reviews from './components/ReviewList/ReviewList';
 
 function App() {
   return (
     <div>
       <Form/>
+      <Reviews />
+      <ReviewItem />
     </div>
   );
 }
---
+++ src/components/ReviewList/ReviewList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./ReviewList.module.css";
+import user from "../../media/user.svg";
+
+const Reviews = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Reviews;
